43d52e436bab8432fe5ac6986f4f4b511571fafb
shivam.kumar <shivam.designweb@gmail.com>
2020-01-24 00:37:43 +0530

    add public folder for zeit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'none',
     entry : ["whatwg-fetch", "./src/index.js"],
     output:{
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: "bundle.js",
         publicPath: '/'
     },
